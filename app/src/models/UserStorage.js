2163bd15c7e651cb13ef91a3140f6c4762cc31b9
Banco <byoung@banco.id>
2021-10-27 11:48:33 +0900

    fs모듈을 통한 로그인 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,20 @@
 'use strict';
 
+const fs = require('fs').promises;
+
 class UserStorage {
-    // 사용자 정보
-    static #users = {
-        id: ['vericras', 'zizimoos'],
-        pw: ['1234', '12345'],
-    };
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const userInfo = Object.keys(users).reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
+    }
 
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -20,18 +26,16 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const userInfo = Object.keys(users).reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+        return fs
+            .readFile('./src/databases/vericras/user.json')
+            .then(data => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(err => console.log);
     }
 
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.userID);
         users.pw.push(userInfo.userPw);
         users.name.push(userInfo.userName);
